ed35d6658affd1326dc90932b2bb868be4dcdfb6
Damian <swean@vp.pl>
2025-10-06 23:29:48 +0200

    issue Page

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuePage = () => {
-	return <div><Button>New Issue</Button></div>;
+	return <div><Button><Link href="/issues/new">New Issue</Link></Button></div>;
 };
 
 export default IssuePage;
--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<Theme>
-				<main>
+				<main className='p-5'>
 					<NavBar />
 					{children}
 				</main>
