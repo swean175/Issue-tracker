5a6d42ba5867cffab33c69b8780cecfc2cd1105d
Damian <swean@vp.pl>
2025-10-08 01:29:01 +0200

    errors
    Handling Errors

--- issue-tracker/app/api/issues/route.tsx
+++ issue-tracker/app/api/issues/route.tsx
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prisma } from "@/prisma/client";
 
-const createIssueSchema =z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+const createIssueSchema = z.object({
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Description is required'),
 })
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    createIssueSchema.parse(body);
+    
     const validation = createIssueSchema.safeParse(body);
-    if(!validation.success) {
-        return NextResponse.json("validation.error.errors", {status: 400});
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), {status: 400});
 
     } else {
 
--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,14 +1,47 @@
 'use client';
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+
+import { useRouter } from "next/navigation";
+import { TextField,  Button, Callout } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
 import React from "react";
+interface IssueForm {
+    title: string;
+    description: string;
+}
+
 const NewIssuePage = () => {
+    const router = useRouter()
+    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const [error, setError] = React.useState('')
+
     return (
-        <div className='max-w-xl space-y-3'>
-            <TextField.Root placeholder="Title"/>
-            <TextArea placeholder="Desciption" />
-            <Button>Submit New Issue</Button>
+        <div  className='max-w-xl'>
+
+      {error && <Callout.Root color="red" className="mb-4">
+            <Callout.Text>
+                {error}
+            </Callout.Text>
+        </Callout.Root>}
+        <form 
+        className='space-y-3' onSubmit={handleSubmit(async (data) => {
+            try {  await axios.post('/api/issues', data);
+                router.push('/issues');} 
+                catch (error) {
+                setError('Failed to create issue');
+            }
+        })}>
+            <TextField.Root placeholder="Title" {...register("title")}/>
+            <Controller
+                name="description"
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder="Desciption" {...field}/>}/>
+            <Button type='submit'>Submit New Issue</Button>
         
-        </div>
+        </form>
+          </div>
     );
 };
 
