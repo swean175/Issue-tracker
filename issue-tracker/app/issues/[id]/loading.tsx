263f9ec6b3a0f937c0d0274f57e7ec722ac408f7
Damian <swean@vp.pl>
2025-10-14 23:14:13 +0200

    IssueDetailPage

---
+++ issue-tracker/app/issues/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingIssueDetailPage = () => {
+  return (
+    <div>
+      <h2>LoadingIssueDetailPage</h2>
+    </div>
+  );
+};
+
+export default LoadingIssueDetailPage;
--- issue-tracker/app/issues/new/loading.tsx
+++ issue-tracker/app/issues/new/loading.tsx
@@ -1,38 +1,11 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import { Table } from '@radix-ui/themes';
 import React from 'react';
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-import IssueActions from '../IssueActions';
 
-const LoadingIssuePage = () => {
+const LoadingNewIssuePage = () => {
+  return (
+    <div>
+      <h2>Loading.....</h2>
+    </div>
+  );
+};
 
-    const issues = [1, 2, 3, 4, 5]
-    return (
-        <div>
-            
-            <IssueActions/>
-            <Table.Root variant='surface'>
-                    <Table.Header>
-                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
-                    </Table.Header>
-                    <Table.Body>
-                        {issues.map((issue) => (
-                            <Table.Row key={issue}>
-                                <Table.Cell><Skeleton />
-                                    <div className="block md:hidden"><Skeleton /></div>
-                                </Table.Cell>
-                                <Table.Cell className="hidden md:table-cell"><Skeleton /></Table.Cell>
-                                <Table.Cell className="hidden md:table-cell"><Skeleton /></Table.Cell>
-                            </Table.Row>
-                        ))}
-                                        
-                    </Table.Body>
-                </Table.Root>
-            </div>
-            )
-}
-
-export default LoadingIssuePage;
+export default LoadingNewIssuePage;
