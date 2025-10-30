d5d796d26a3160a8a4c57672f758988780f0c15f
Damian <swean@vp.pl>
2025-10-30 20:30:03 +0100

    Update Issue
    new feature

--- issue-tracker/app/issues/_components/IssueFrom.tsx
+++ issue-tracker/app/issues/_components/IssueFrom.tsx
@@ -38,8 +38,14 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 	const onSubmit = handleSubmit(async (data) => {
 		try {
 			setSubmitting(true);
-			await axios.post("/api/issues", data);
-			router.push("/issues");
+			if (issue) {
+				await axios.patch(`/api/issues/${issue.id}`, data);
+				return;
+			} else {
+				await axios.post("/api/issues", data);
+				router.push("/issues");
+			}
+
 		} catch (error) {
 			setSubmitting(false);
 			setError("Failed to create issue");
@@ -65,7 +71,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 				/>
 				<ErrorMessage>{errors.description?.message}</ErrorMessage>
 				<Button disabled={isSubmitting} type="submit">
-					Submit New Issue{isSubmitting && <Spinner />}
+					{issue ? "Update Issue" : "Submit New Issue"}{" "}
+					{isSubmitting && <Spinner />}
 				</Button>
 			</form>
 		</div>
