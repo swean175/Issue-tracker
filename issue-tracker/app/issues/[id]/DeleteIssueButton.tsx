64315dfbb90e105516b6ba71a027ee6c4e4a5947
Damian <swean@vp.pl>
2025-11-23 19:42:26 +0100

    Delete Alert

--- issue-tracker/app/issues/[id]/DeleteIssueButton.tsx
+++ issue-tracker/app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,36 @@
 'use client';
-import { Button } from '@radix-ui/themes';
+import { Button, AlertDialog, Flex } from '@radix-ui/themes';
 import React from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
     <div>
-      <Button color="red" >Delete</Button>
+      <AlertDialog.Root>
+	<AlertDialog.Trigger>
+		<Button color="red" >Delete</Button>
+	</AlertDialog.Trigger>
+	<AlertDialog.Content maxWidth="450px">
+		<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+		<AlertDialog.Description size="2">
+			Are you sure you want deleter this issue? 
+		</AlertDialog.Description>
+
+		<Flex gap="3" mt="4" justify="end">
+			<AlertDialog.Cancel>
+				<Button variant="soft" color="gray">
+					Cancel
+				</Button>
+			</AlertDialog.Cancel>
+			<AlertDialog.Action>
+				<Button variant="solid" color="red">
+					Dlete Issue
+				</Button>
+			</AlertDialog.Action>
+		</Flex>
+	</AlertDialog.Content>
+</AlertDialog.Root>
+
+     
     </div>
   );
 };
